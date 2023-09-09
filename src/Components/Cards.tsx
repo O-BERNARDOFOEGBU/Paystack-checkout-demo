53512b2dd34039bb03245145a8ab8082c823d4c1
O-Bernard Ofoegbu <obinnaofoegbu71@gmail.com>
2023-09-09 15:58:33 +0100

    optimal responsiveness

--- src/Components/Cards.tsx
+++ src/Components/Cards.tsx
@@ -16,35 +16,31 @@ const Cards = ({
   const { name, price, img } = item;
 
   return (
-    <>
-      <section className="flex flex-col md:flex-row px-2 py-2 md:px-6 md:py-4 lg:w-1/4 w-full">
-        <div className="p-2 md:w-1/2 w-full mb-4">
-          <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-auto">
-            <img
-              className="md:h-48 h-36 w-full object-cover object-center"
-              src={img}
-              alt="item"
-            />
-            <div className="px-2 py-2 md:px-3 md:py-2">
-              <h1 className="text-base md:text-lg lg:text-xl font-bold mb-2 md:mb-3">
-                {name}
-              </h1>
-              <div className="flex flex-wrap justify-between mb-2 mt-2 md:mt-4">
-                <p className="leading-relaxed mt-2 md:mt-4 text-sm md:text-base lg:text-lg">
-                  Price: NGN{price}
-                </p>
-                <button
-                  onClick={() => handleClick(item)}
-                  className="bg-green-400 text-white py-2 px-2 md:px-3 text-sm md:text-base lg:text-lg rounded-xl hover:border-2 hover:bg-green-800 hover:text-brandColor hover:border-brandColor"
-                >
-                  Add to Cart
-                </button>
-              </div>
-            </div>
+    <div className="lg:w-1/4 md:w-1/2 w-full p-2">
+      <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden transition-transform transform hover:scale-105">
+        <img
+          className="h-48 w-full object-cover object-center"
+          src={img}
+          alt="item"
+        />
+        <div className="p-3">
+          <h1 className="text-lg md:text-xl font-bold mb-2 md:mb-3">
+            {name}
+          </h1>
+          <div className="flex flex-wrap justify-between mb-2 mt-2 md:mt-4">
+            <p className="leading-relaxed mt-2 md:mt-4 text-base md:text-lg">
+              Price: NGN{price}
+            </p>
+            <button
+              onClick={() => handleClick(item)}
+              className="bg-green-400 text-white py-2 px-2 md:px-3 text-base md:text-lg rounded-xl hover:border-2 hover:bg-green-800 hover:text-brandColor hover:border-brandColor"
+            >
+              Add to Cart
+            </button>
           </div>
         </div>
-      </section>
-    </>
+      </div>
+    </div>
   );
 };
 
--- src/Components/Details.tsx
+++ src/Components/Details.tsx
@@ -1,103 +1,121 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Cards from "./Cards";
 import { list } from "../data";
 import { FaSearch } from "react-icons/fa";
 
+interface CartItem {
+ id: string;
+ img: string;
+ name: string;
+ amount: number;
+ price: number;
+}
+
 interface DetailsProps {
-  handleClick: (item: any) => void;
+ handleClick: (item: any) => void;
 }
 
 function Details({ handleClick }: DetailsProps) {
-  const [category, setCategory] = useState(list);
-  const [activeTab, setActiveTab] = useState("All");
+ const [category, setCategory] = useState(list);
+ const [activeTab, setActiveTab] = useState("All");
 
-  // Search functionality
-  const [query, setQuery] = useState("");
+ //search functionality
+ const [query, setQuery] = useState("");
 
-  // Filtering functionality
-  const handleBtns = (word: string) => {
-    if (word === "All") {
-      setCategory(list);
-    } else {
-      const filtered = list.filter((item) => item.kind === word);
-      setCategory(filtered);
-    }
+ //filtering functionality
+ const handleBtns = (word: string) => {
+   if (word === "All") {
+     setCategory(list);
+   } else {
+     const filtered = list.filter((item) => item.kind === word);
+     setCategory(filtered);
+   }
 
-    setActiveTab(word);
-  };
+   setActiveTab(word);
+ };
 
-  return (
-    <>
-      <section className="container pt-4 mx-auto w-full bg-bgColor">
-        <section className="px-4 md:px-6 flex flex-col md:flex-row justify-between">
-          <div className="relative w-full md:w-80 h-11 mt-4 mb-4 md:mb-0 ">
-            <input
-              type="text"
-              onChange={(event) => setQuery(event.target.value)}
-              className="w-full h-full py-2 md:py-4 px-4 md:px-10 text-base text-black rounded-lg border-2 border-black pl-10"
-              placeholder="Search food..."
-            />
-            <i className=" absolute left-2 top-2 md:left-4 pt-1 md:top-6 text-lg w-4 h-4 text-center text-black focus:outline-none">
-              <FaSearch />
-            </i>
-          </div>
+ return (
+   <>
+     <section className="container pt-4 mx-auto w-full bg-bgColor">
+       <section className="px-6 flex lg:flex-row md:flex-row justify-between md:flex flex-col">
+         <div className="relative w-80 h-11 mt-4 md:w-60 md:mr-2">
+           <input
+             type="text"
+             onChange={(event) => setQuery(event.target.value)}
+             className="w-full h-full py-4 px-10 text-base text-black rounded-lg border-2 border-black"
+             placeholder="Search food..."
+           />
+           <i>
+             <FaSearch className="absolute left-4 top-4 text-lg w-4 h-4 text-center text-black focus:outline-none" />
+           </i>
+         </div>
 
-          <div className="flex flex-wrap mt-4 md:mt-0">
-            <button
-              value="All"
-              onClick={() => handleBtns("All")}
-              className={`mr-2 mb-2 text-brandColor border-brandColor border-2 py-1 px-3 md:px-6 md:w-24 h-10 rounded-lg text-base md:text-lg ${
-                activeTab === "All" ? "bg-green-400 outline-none text-white" : ""
-              }`}
-            >
-              All
-            </button>
-            <button
-              value="African"
-              onClick={() => handleBtns("African")}
-              className={`mr-2 mb-2 text-brandColor border-brandColor border-2 py-1 px-3 md:px-6 md:w-24 h-10 rounded-lg text-base md:text-lg ${
-                activeTab === "African" ? "bg-green-400 outline-none text-white" : ""
-              }`}
-            >
-              African
-            </button>
-            <button
-              value="American"
-              onClick={() => handleBtns("American")}
-              className={`mr-2 mb-2 text-brandColor border-brandColor border-2 py-1 px-3 md:px-6 md:w-24 h-10 rounded-lg text-base md:text-lg ${
-                activeTab === "American" ? "bg-green-400 outline-none text-white" : ""
-              }`}
-            >
-              American
-            </button>
-            <button
-              value="Chinese"
-              onClick={() => handleBtns("Chinese")}
-              className={`mr-2 mb-2 text-brandColor border-brandColor border-2 py-1 px-3 md:px-6 md:w-24 h-10 rounded-lg text-base md:text-lg ${
-                activeTab === "Chinese" ? "bg-green-400 outline-none text-white" : ""
-              }`}
-            >
-              Chinese
-            </button>
-          </div>
-        </section>
+         <div className="flex flex-wrap mt-4 lg:mb-4 mb-8">
+           <button
+             value="All"
+             onClick={() => handleBtns("All")}
+             className={`mr-2 text-brandColor border-brandColor border-2 py-1 px-6 md:w-24 h-10 rounded-lg text-lg hover:bg-green-400 hover:text-white hover:border-green-400 ${
+               activeTab === "All"
+                 ? "bg-green-400 outline-none text-white"
+                 : ""
+             }`}
+           >
+             All
+           </button>
+           <button
+             value="African"
+             onClick={() => handleBtns("African")}
+             className={`mr-2 text-brandColor border-brandColor border-2 py-1 px-6 md:w-24 h-10 rounded-lg text-lg hover:bg-green-400 hover:text-white hover:border-green-400 ${
+               activeTab === "African"
+                 ? "bg-green-400 outline-none text-white"
+                 : ""
+             }`}
+           >
+             African
+           </button>
+           <button
+             value="American"
+             onClick={() => handleBtns("American")}
+             className={`mr-2 text-brandColor border-brandColor border-2 py-1 md:w-24 h-10 rounded-lg text-lg hover:bg-green-400 hover:text-white hover:border-green-400 ${
+               activeTab === "American"
+                 ? "bg-green-400 outline-none text-white"
+                 : ""
+             }`}
+           >
+             American
+           </button>
+           <button
+             value="Chinese"
+             onClick={() => handleBtns("Chinese")}
+             className={`mr-2 text-brandColor border-brandColor border-2 py-1 md:w-24 h-10 rounded-lg text-lg hover:bg-green-400 hover:text-white hover:border-green-400 ${
+               activeTab === "Chinese"
+                 ? "bg-green-400 outline-none text-white"
+                 : ""
+             }`}
+           >
+             Chinese
+           </button>
+         </div>
+       </section>
 
-        <section className="flex flex-col mt-4 md:mt-0">
-          {category
-            .filter((name) => {
-              if (query === "") {
-                return true; // Return true when the query is empty
-              } else {
-                return name.name.toLowerCase().includes(query.toLowerCase());
-              }
-            })
-            .map((item) => (
-              <Cards key={item.id} item={item} handleClick={handleClick} />
-            ))}
-        </section>
-      </section>
-    </>
-  );
+       <section className="flex flex-row flex-wrap">
+         {category
+           .filter((name) => {
+             if (query === "") {
+               return name;
+             } else if (
+               name.name.toLowerCase().includes(query.toLowerCase())
+             ) {
+               return name;
+             }
+           })
+           .map((item) => (
+             <Cards key={item.id} item={item} handleClick={handleClick} />
+           ))}
+       </section>
+     </section>
+   </>
+ );
 }
 
 export default Details;
